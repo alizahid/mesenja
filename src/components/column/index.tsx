3e09dbab1a54175a1ea96c4086c792b46b6f79ea
Ali Zahid <ali.zahid@live.com>
2019-05-04 13:26:50 +0400

    Remember scroll position for columns

--- src/components/column/index.tsx
+++ src/components/column/index.tsx
@@ -1,6 +1,9 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useRef } from 'react'
+import { get } from 'lodash'
 
 import { Post as PostI } from '../../store/models/posts'
+
+import { useActions, useStore } from '../../store'
 
 import Post from '../post'
 
@@ -12,10 +15,32 @@ interface Props {
 }
 
 const Column: FunctionComponent<Props> = ({ posts, title }) => {
+  const column = useRef<HTMLInputElement>(null)
+
+  const scroll = useStore(state => state.nav.scroll)
+  const updateScroll = useActions(actions => actions.nav.setScroll)
+
+  useEffect(() => {
+    if (column.current) {
+      const position = scroll[title]
+
+      column.current.scrollTo({
+        top: position
+      })
+    }
+
+    return () => {
+      updateScroll({
+        column: title,
+        position: get(column, 'current.scrollTop')
+      })
+    }
+  }, [title, scroll, updateScroll])
+
   return (
     <section className="posts">
       <h2>{title}</h2>
-      <section>
+      <section ref={column}>
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
--- src/store/models/nav.ts
+++ src/store/models/nav.ts
@@ -5,13 +5,25 @@ import { Team } from './teams'
 
 export interface NavModel {
   team?: Team
+  scroll: {
+    [key: string]: number
+  }
 
   setTeam: Thunk<NavModel, string, any, StoreModel>
   updateTeam: Action<NavModel, Team>
+
+  setScroll: Action<
+    NavModel,
+    {
+      column: string
+      position: number
+    }
+  >
 }
 
 const nav: NavModel = {
   team: undefined,
+  scroll: {},
 
   setTeam: thunk(async (actions, payload, { getStoreState }) => {
     const {
@@ -30,6 +42,10 @@ const nav: NavModel = {
   }),
   updateTeam: action((state, payload) => {
     state.team = payload
+  }),
+
+  setScroll: action((state, { column, position }) => {
+    state.scroll[column] = position
   })
 }
 
